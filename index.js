1bdeec41eb0a10555102b1172b23506eba905d4b
Tony Petty <tpetty@appmobi.com>
2013-11-17 22:42:51 -0500

    Added some jquery mobile css for the panel. Modified javascript and other css a bit adding features.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*global $ */
+/*global $, alert */
 $(document).ready(function () {
 	var height, html5audio, sourceel;
 	document.body.addEventListener('touchstart', function (e) { e.preventDefault(); });
@@ -8,6 +8,7 @@ $(document).ready(function () {
 		//html5audio.playclip();
 		$('#display').val('');
 		$('.number').removeClass('disabled');
+		$('#save').addClass('disabled');
 	});
 	$('.number').on('vclick', function () {
 		var number = $(this).text();
@@ -18,10 +19,19 @@ $(document).ready(function () {
 				if (value === '' && number === '.') {
 					value = '0';
 				}
+				if (number === '.') {
+					$('#point').addClass('disabled');
+				}
 				out = value + String(number);
 			}
-			if (out.search(/^\d*\.\d{2}$/) !== -1) {
+			if (out.indexOf('-') === -1) {
+				out = '-' + out;
+			}
+			if (out.search(/^-\d*\.\d{2}$/) !== -1) {
 				$('.number').addClass('disabled');
+			}
+			if (parseFloat(out) < 0) {
+				$('#save').removeClass('disabled');
 			}
 			return out;
 		});
@@ -37,4 +47,32 @@ $(document).ready(function () {
 		html5audio.currentTime = 0;
 		html5audio.play();
 	};
+	$.getJSON('api.php', {
+		'action' : 'getbalance',
+		'account_name' : 'checking_bill'
+	}, function (data) {
+		if (!data.error) {
+			$('#display').attr('placeholder', data.balance);
+		}
+	});
+	$('#save').on('vclick', function () {
+		console.log('saving');
+		$.getJSON('api.php', {
+			'action' : 'addtransaction',
+			'account_name' : 'checking_bill',
+			'amount' : $('#display').val()
+		}, function (data) {
+			console.log(data);
+			if (!data.error) {
+				$('#display').attr('placeholder', data.balance).val('');
+				$('.number').removeClass('disabled');
+				$('#save').addClass('disabled');
+			} else {
+				alert('Error saving transaction');
+			}
+		});
+	});
+	$(document).on('swiperight', function () {
+		$("#panel").panel("open");
+	});
 });
--- index.php
+++ index.php
@@ -1,73 +1,79 @@
-<?php
-?>
+<?php ?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>Simple Calc</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0.0">
+		<link rel="stylesheet" href="jquery.mobile-1.3.2-modified.css">
+		<!-- <link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.2/jquery.mobile.structure-1.3.2.css"> -->
 		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css">
 		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap-theme.min.css">
 		<link rel="stylesheet" href="index.css">
-	</head>
-	<body>
-		<div class="main-container">
-			<div class="row">
-				<div class="col-xs-4 no-padding">
-					<button id="clear" type="button" class="btn btn-default btn-lg btn-block btn-warning">Clear</button>
-				</div>
-				<div class="col-xs-8 no-padding">
-					<input id="display" type="text" class="form-control input-lg text-right" disabled placeholder="0.00">
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-xs-4 no-padding">
-					<button type="button" class="btn btn-default btn-lg btn-block number btn-primary">1</button>
-				</div>
-				<div class="col-xs-4 no-padding">
-					<button type="button" class="btn btn-default btn-lg btn-block number btn-primary">2</button>
-				</div>
-				<div class="col-xs-4 no-padding">
-					<button type="button" class="btn btn-default btn-lg btn-block number btn-primary">3</button>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-xs-4 no-padding">
-					<button type="button" class="btn btn-default btn-lg btn-block number btn-primary">4</button>
-				</div>
-				<div class="col-xs-4 no-padding">
-					<button type="button" class="btn btn-default btn-lg btn-block number btn-primary">5</button>
-				</div>
-				<div class="col-xs-4 no-padding">
-					<button type="button" class="btn btn-default btn-lg btn-block number btn-primary">6</button>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-xs-4 no-padding">
-					<button type="button" class="btn btn-default btn-lg btn-block number btn-primary">7</button>
-				</div>
-				<div class="col-xs-4 no-padding">
-					<button type="button" class="btn btn-default btn-lg btn-block number btn-primary">8</button>
-				</div>
-				<div class="col-xs-4 no-padding">
-					<button type="button" class="btn btn-default btn-lg btn-block number btn-primary">9</button>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-xs-4 no-padding">
-					<button type="button" class="btn btn-default btn-lg btn-block number btn-primary">0</button>
-				</div>
-				<div class="col-xs-4 no-padding">
-					<button type="button" class="btn btn-default btn-lg btn-block number btn-primary">.</button>
-				</div>
-				<div class="col-xs-4 no-padding">
-					<button id="save" type="button" class="btn btn-default btn-lg btn-block btn-success">Save</button>
-				</div>
-			</div>
-		</div>
-
 		<script src="//code.jquery.com/jquery.min.js"></script>
 		<script src="//code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
 		<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js"></script>
 		<script src="index.js"></script>
+	</head>
+	<body>
+		<div data-role="page" class="main-container">
+			<div data-role="panel" id="panel">
+				asdf
+			</div>
+			<div data-role="content">
+				<div class="row">
+					<div class="col-xs-4 no-padding">
+						<button id="clear" type="button" class="btn btn-default btn-lg btn-block btn-warning">Clear</button>
+					</div>
+					<div class="col-xs-8 no-padding">
+						<input id="display" type="text" class="form-control input-lg text-right" disabled
+						placeholder="0.00">
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-4 no-padding">
+						<button type="button" class="btn btn-default btn-lg btn-block number btn-primary">1</button>
+					</div>
+					<div class="col-xs-4 no-padding">
+						<button type="button" class="btn btn-default btn-lg btn-block number btn-primary">2</button>
+					</div>
+					<div class="col-xs-4 no-padding">
+						<button type="button" class="btn btn-default btn-lg btn-block number btn-primary">3</button>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-4 no-padding">
+						<button type="button" class="btn btn-default btn-lg btn-block number btn-primary">4</button>
+					</div>
+					<div class="col-xs-4 no-padding">
+						<button type="button" class="btn btn-default btn-lg btn-block number btn-primary">5</button>
+					</div>
+					<div class="col-xs-4 no-padding">
+						<button type="button" class="btn btn-default btn-lg btn-block number btn-primary">6</button>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-4 no-padding">
+						<button type="button" class="btn btn-default btn-lg btn-block number btn-primary">7</button>
+					</div>
+					<div class="col-xs-4 no-padding">
+						<button type="button" class="btn btn-default btn-lg btn-block number btn-primary">8</button>
+					</div>
+					<div class="col-xs-4 no-padding">
+						<button type="button" class="btn btn-default btn-lg btn-block number btn-primary">9</button>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-4 no-padding">
+						<button type="button" class="btn btn-default btn-lg btn-block number btn-primary">0</button>
+					</div>
+					<div class="col-xs-4 no-padding">
+						<button id="point" type="button" class="btn btn-default btn-lg btn-block number btn-primary">.</button>
+					</div>
+					<div class="col-xs-4 no-padding">
+						<button id="save" type="button" class="btn btn-default btn-lg btn-block btn-success disabled">Save</button>
+					</div>
+				</div>
+			</div>
+		</div>
 	</body>
 </html>
